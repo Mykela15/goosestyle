84eba2626167e64c579330859dda395d6972972b
Mykela Greenwood <givemespiders@hotmail.com>
2016-06-11 17:40:42 +0000

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+## Cashing the Inverse of a Square Matrix
+## 1.) make CacheMatrix creates a special matrix obj
+## 2.) cacheSolve calculates the inverse of the matrix
+## 3.) test if matrix inverse has already been calculated
+##      a.) if yes it will find cache and return it
+##      b.) if not it will calculate it again
+
+
+makeCacheMatrix <- function(x = matrix()) {
+	invrs <- NULL
+	set <- function(y){
+		x <- y
+		invrs <- NULL
+}	
+	get <- function()x
+	setinverse <- function(inverse)invrs <- inverse
+	getinverse <- function() invrs
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+## cacheSolve does steps 2 and 3 listed above
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+	invrs <- x$getinverse()
+	if(!is.null(invrs)) {
+	message("getting cached data")
+	return(invrs)
+}
+}
